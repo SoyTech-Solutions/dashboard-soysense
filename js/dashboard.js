6e7117e7687fa2493320ff6e9227a9dbf693f0ef
jeancharlesx64 <jean.albuquerque@sptech.school>
2024-04-20 16:50:09 -0300

    side-bar finished

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,7 +1,6 @@
 var accordionItemList = document.querySelectorAll('.accordion-item');
 
 
-
 for(let i = 0; i <= accordionItemList.length; i++){
     accordionItemList[i].addEventListener('click', ()=>{
         accordionItemList[i].classList.toggle('drop-active');
