9fd1cb3b83a577ef53c9ddfc7c6ba6c2ff00bc68
jeancharlesx64 <jean.albuquerque@sptech.school>
2024-04-26 23:41:19 -0300

    adding monitor screen, search bar, fixing accordion again

--- js/side-bar.js
+++ js/side-bar.js
@@ -1,7 +1,7 @@
 var accordionItemList = document.querySelectorAll('.accordion-item');
 
 
-for(let i = 0; i <= accordionItemList.length; i++){
+for(let i = 0; i < accordionItemList.length; i++){
     accordionItemList[i].addEventListener('click', ()=>{
         accordionItemList[i].classList.toggle('drop-active');
     })
