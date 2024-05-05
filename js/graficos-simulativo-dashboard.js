385fa17e7e6b93a2f83ad9f3a159accaaeae037f
Natan-pinheiro <natan.pinheiro@sptech.school>
2024-05-05 04:14:32 -0300

    Adicionando gráfico de desempenho - Natan

--- js/graficos-simulativo-dashboard.js
+++ js/graficos-simulativo-dashboard.js
@@ -20,3 +20,42 @@ setInterval(() => {
     tituloDesempenho.innerHTML = `${desempenho}%`;
 
 }, 1000);
+
+/*----Desempenho total por mes */
+var contextoDesempenhoT = document.getElementById('desempenhoT').getContext('2d');
+var desempenhoT = new Chart(
+    contextoDesempenhoT, 
+    {
+        type: 'bar',
+        data: {
+            labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 
+                    'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+            datasets: [{
+            label: 'Desempenho',
+            data: [90, 90, 88, 79, 93, 95, 78, 81, 70, 88, 83, 90],
+            backgroundColor:
+            [
+                '#1d7fff',
+                '#1d7fff',
+                '#1d7fff',
+                '#1d7fff',
+                '#1d7fff',
+                '#1d7fff',
+                '#1d7fff',
+                '#1d7fff',
+                '#1d7fff',
+                '#1d7fff',
+                '#1d7fff',
+                '#1d7fff',
+            ],
+            borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+            y: {
+                beginAtZero: true
+            }
+            }
+        }
+});
